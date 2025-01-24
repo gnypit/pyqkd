a0182ad275710aa2c3a44734e5d259c61ad9bab4
Jakub T. Gnyp <gnyp.jakub@gmail.com>
2025-01-24 11:52:02 +0100

    Added first simple test of the genetic algorithm - solving an optimisation problem with an exp/sin function in comparison to pygad.

---
+++ genal/material_endurance_test.py
@@ -0,0 +1,64 @@
+import math
+import genetic_algorithm
+import selection_operators
+import crossover_operators
+import pygad
+import numpy as np
+
+
+def endurance(x, y, z, u, v, w):
+    """Function to be optimised"""
+    return math.exp(-2 * (y - math.sin(x)) ** 2) + math.sin(z * u) + math.cos(v * w)
+
+
+def fitness_function_pygad(genetic_algorithm_instance, chromosome, chromosome_idx):
+    x = chromosome[0]
+    y = chromosome[1]
+    z = chromosome[2]
+    u = chromosome[3]
+    v = chromosome[4]
+    w = chromosome[5]
+
+    fitness_value = endurance(x=x, y=y, z=z, u=u, w=w, v=v)
+
+    return fitness_value
+
+
+def fitness_function(chromosome):
+    x = chromosome[0]
+    y = chromosome[1]
+    z = chromosome[2]
+    u = chromosome[3]
+    v = chromosome[4]
+    w = chromosome[5]
+
+    fitness_value = endurance(x=x, y=y, z=z, u=u, w=w, v=v)
+
+    return fitness_value
+
+
+if __name__ == '__main__':
+    """Firstly, solving problem with pygad:"""
+    ga_instance = pygad.GA(
+        gene_space=np.linspace(start=0, stop=1, num=100000),
+        num_generations=70,
+        num_parents_mating=9,
+        fitness_func=fitness_function,
+        sol_per_pop=18,
+        num_genes=6,
+        parent_selection_type='tournament',
+        mutation_type='random',
+        mutation_probability=float(1 / 6),
+        stop_criteria=['reach_2.83', 'saturate_15']
+    )
+
+    ga_instance.run()
+
+    solution, solution_fitness, solution_idx = ga_instance.best_solution()
+    generations_number = ga_instance.best_solution_generation
+
+    print(f"Best solution: {solution}")
+    print(f"Fitness value of the best solution: {solution_fitness}")
+    print(f"It took {generations_number} generations to find an optimal solution.")
+
+    ga_instance.plot_fitness()
