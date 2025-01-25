ae2a5a5547e8801ffa3e8ad43edd9f9665513fb9
Jakub T. Gnyp <gnyp.jakub@gmail.com>
2025-01-25 15:02:23 +0100

    Deleted labyrinth_original.ipynb and labyrinth_test.ipynb, because each run creates new metadata tracked by Git.
    Updated and translated comments in PyGAD approach to solving a labyrinth in the labyrinth_test.py script.

--- genal/labyrinth_test.py
+++ genal/labyrinth_test.py
@@ -1,3 +1,7 @@
+"""Jakub T. Gnyp, gnyp.jakub@gmail.com
+Labyrinth test case for the Genetic Algorithm - script with functions for visualizations.
+Required program: ImageMagick https://www.techspot.com/downloads/5515-imagemagick.html
+"""
 import genetic_algorithm
 import selection_operators
 import crossover_operators
@@ -6,11 +10,17 @@ import random
 import tqdm
 import copy
 import numpy as np
+import plotly.express as px
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 import matplotlib.animation as animation
 
 from time import time
+
+"""The labyrinth is encoded using a matrix with elements 0 and 1. A zero represents an allowed field that can be 
+traversed, while a black field represents a wall; these fields cannot be entered. Including the walls on the boundary of 
+the labyrinth, its dimensions are 12x12, so the coordinates in Python will be numbered from 0 to 11. The entrance is 
+located at field (1,1), while the exit is at field (10,10)."""
 
 labyrinth = np.array([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -27,107 +37,16 @@ labyrinth = np.array([
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 ])
 
+"""The movements through the labyrinth are encoded as follows:  
+0 - no movement;  
+1 - move left;  
+2 - move right;  
+3 - move up;  
+4 - move down.  
 
-def draw_labyrinth(plot_object, labyrinth: np.ndarray):
-    """Funkcja rysująca pusty labirynt (bez trasy) zgodnie z macierzą 'labyrinth', na podanym obiekcie
-    graficznym 'plot_object'.
-    """
-    for i in range(len(labyrinth)):
-        """Iteracja po kolejnych wierszach"""
-        for j in range(len(labyrinth[i])):
-            """Iteracja po kolejnych kolumnach"""
-            if labyrinth[i, j] == 1:
-                """Rysowanie ściany"""
-                rect = patches.Rectangle(
-                    (j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black'
-                )
-            else:
-                """Rysowanie pustego pola"""
-                rect = patches.Rectangle(
-                    (j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white'
-                )
-            plot_object.add_patch(rect)
-
-    """Końcowe ustawienia"""
-    plot_object.set_xlim(0, 12)
-    plot_object.set_ylim(0, 12)
-    plot_object.invert_yaxis()
-    plot_object.set_aspect('equal')
-
-
-def see_route(labyrinth: np.ndarray, moves_mapping: dict, steps: list,
-              gif_filename='labirynt.gif', summary_filename='labirynt_summary.png'):
-    """Funkcja przyjmująca na wejściu macierz reprezentującą labirynt (labyrinth), słownik dopasowujący kod ruchu
-    do zmiany odpowiednich współrzędnych — w macierzy z biblioteki numpy najpierw jest wsp. wiersza, a następnie wsp.
-    kolumny!
-
-    Wynikiem funkcji jest animacja GIF danej trasy przez labirynt oraz grafika PNG z podsumowaniem całej trasy.
-    """
-    start_pos = (1, 1)
-    fig, ax = plt.subplots()
-
-    """Rysowanie pustego labiryntu"""
-    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)
-
-    """Tworzenie trasy na podstawie kroków"""
-    path_x, path_y = [start_pos[1] + 0.5], [start_pos[0] + 0.5]  # ścieżka zaczyna się w środku pola wejścia, stąd 0.5
-    pos = list(start_pos)
-    for step in steps:
-        move = moves_mapping[int(step)]
-        pos[0] += move[0]
-        pos[1] += move[1]
-        path_x.append(pos[1] + 0.5)  # przesunięcie o 0.5, żeby linie były "wycentrowane", a nie wzdłuż krawędzi pól
-        path_y.append(pos[0] + 0.5)  # przesunięcie o 0.5, żeby linie były "wycentrowane", a nie wzdłuż krawędzi pól
-
-    def update(num, path_x, path_y, line):  # wewnątrz funkcji można tworzyć "lokalną" funkcję
-        """Funkcja do aktualizacji animacji"""
-        line.set_data(path_x[:num], path_y[:num])
-        return line,
-
-    """Inicjalizacja linii trasy"""
-    line, = ax.plot([], [], lw=2, color='red')
-
-    """Animacja trasy wzdłuż odpowiednich współrzędnych z zadanym interwałem, określającym jak szybko zmieniają się
-    klatki w końcowym GIFie."""
-    ani = animation.FuncAnimation(fig, update, frames=len(path_x), fargs=[path_x, path_y, line], interval=200,
-                                  blit=True)
-
-    """Ustawienia osi"""
-    plt.xlim(0, 12)
-    plt.ylim(0, 12)
-    plt.gca().invert_yaxis()
-
-    """Zapisanie animacji jako GIF"""
-    ani.save(gif_filename, writer='imagemagick')
-    # print(f"Wygenerowano animację trasy przez labirynt")
-
-    """Rysowanie całej trasy na grafice PNG"""
-    fig, ax = plt.subplots()  # wykorzystuję ponownie zmienne lokalne
-    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)  # rysujemy pusty labirynt od nowa
-    ax.plot(path_x, path_y, lw=2, color='red')  # dodajemy całą wygenerowaną trasę
-
-    """Ustawienia osi"""
-    plt.xlim(0, 12)
-    plt.ylim(0, 12)
-    plt.gca().invert_yaxis()
-
-    """Zapisanie obrazu jako PNG"""
-    plt.savefig(summary_filename)
-    plt.close()
-    # print(f"Wykonano grafikę z całą trasą przez labirynt")
-
-
-"""Ruchy przez labirynt zakodowano w następujący sposób:
-0 - bez ruchu;
-1 - w lewo;
-2 - w prawo;
-3 - w górę;
-4 - w dół.
-
-Dodatkowo, stworzono słownik mapujący to kodowanie do zmian odpowiednich współrzędnych.
-moves_mapping = {ruch: (zmiana y, zmiana x), ...}
-y jest pierwszą wsp. w macierzy, ponieważ to nr wiersza!
-"""
+Additionally, a dictionary has been created to map this encoding to the corresponding coordinate changes:  
+`moves_mapping = {movement: (change in y, change in x), ...}`  
+`y` is the first coordinate in the matrix, as it represents the row number!"""
 gene_space = [0, 1, 2, 3, 4]
 moves_mapping = {
     0: (0, 0),  # bez ruchu
@@ -137,67 +56,162 @@ moves_mapping = {
     4: (1, 0)  # w dół
 }
 
-"""Ustawienia algorytmu genetycznego"""
-exit_labyrinth = {'y': 10, 'x': 10}  # współrzędne "wyjścia" z labiryntu
+
+def draw_labyrinth(plot_object, labyrinth: np.ndarray):
+    """Function draws an empty labyrinth (no path) as dictated by 'labyrinth' matrix, on a provided graphical object
+    'plot_object'."""
+    for i in range(len(labyrinth)):
+        """Iterating over rows"""
+        for j in range(len(labyrinth[i])):
+            """Iterating over columns"""
+            if labyrinth[i, j] == 1:
+                """Drawing walls"""
+                rect = patches.Rectangle(
+                    (j, i), 1, 1, linewidth=1, edgecolor='black', facecolor='black'
+                )
+            else:
+                """Drawing empty fields"""
+                rect = patches.Rectangle(
+                    (j, i), 1, 1, linewidth=1, edgecolor='grey', facecolor='white'
+                )
+            plot_object.add_patch(rect)
+
+    """Some final settings"""
+    plot_object.set_xlim(0, 12)
+    plot_object.set_ylim(0, 12)
+    plot_object.invert_yaxis()
+    plot_object.set_aspect('equal')
+
+
+def see_route(labyrinth: np.ndarray, moves_mapping: dict, steps: list,
+              gif_filename='labirynt.gif', summary_filename='labirynt_summary.png'):
+    """Function takes on input a matrix representing the labyrinth, dict mapping moves inside the labyrinth to changes
+    of coordinates — in matrices from numpy we first have the row (y) coordinate, and then the column coordinate (x)!
+
+    Results of this function are a GIF with a step-by-step animation of a path in the labyrinth
+    and a PNG with the whole path."""
+    start_pos = (1, 1)
+    fig, ax = plt.subplots()
+
+    """Drawing en empty labyrinth"""
+    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)
+
+    """Creating a path based on the following steps"""
+    path_x, path_y = [start_pos[1] + 0.5], [start_pos[0] + 0.5]  # path starts in the middle of a field, hence 0.5
+    pos = list(start_pos)
+    for step in steps:
+        move = moves_mapping[int(step)]
+        pos[0] += move[0]
+        pos[1] += move[1]
+        path_x.append(pos[1] + 0.5)  # 0.5 shift, so that lines are "centered", not along borders of fields
+        path_y.append(pos[0] + 0.5)  # 0.5 shift, so that lines are "centered", not along borders of fields
+
+    def update(num, path_x, path_y, line):
+        """Function for updating the animation."""
+        line.set_data(path_x[:num], path_y[:num])
+        return line,
+
+    """Initiating a line of the path"""
+    line, = ax.plot([], [], lw=2, color='red')
+
+    """Animation of steps with a time interval, specifying FPS in the final GIF."""
+    ani = animation.FuncAnimation(fig, update, frames=len(path_x), fargs=[path_x, path_y, line], interval=200,
+                                  blit=True)
+
+    """Axis settings"""
+    plt.xlim(0, 12)
+    plt.ylim(0, 12)
+    plt.gca().invert_yaxis()
+
+    """Saving animation as a GIF"""
+    ani.save(gif_filename, writer='imagemagick')
+
+    """Drawing the whole path on a PNG"""
+    fig, ax = plt.subplots()  # again using local variables
+    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)  # drawing an empty labyrinth anew
+    ax.plot(path_x, path_y, lw=2, color='red')  # adding the whole generated path
+
+    """Axis settings"""
+    plt.xlim(0, 12)
+    plt.ylim(0, 12)
+    plt.gca().invert_yaxis()
+
+    """Saving the PNG"""
+    plt.savefig(summary_filename)
+    plt.close()
+
+def example():
+    """Example of applying the functions for labyrinth visualisation"""
+    steps = [2, 2, 4, 4, 4, 1, 4, 2, 2, 1, 4, 4, 1, 1, 1, 4, 2, 2, 4, 2, 4, 2]
+    see_route(steps=steps, labyrinth=labyrinth, moves_mapping=moves_mapping)
+    print("see_route executed")
+
+    # Stworzę jeszcze pusty labirynt do analizy
+    fig, ax = plt.subplots()
+    draw_labyrinth(plot_object=ax, labyrinth=labyrinth)
+    plt.savefig("empty_labyrinth.png")
+    print("Empty labyrinth created.")
+
+"""Settings for the genetic algorithm (pygad approach)"""
+exit_labyrinth = {'y': 10, 'x': 10}  # target/exit coordinates of the labyrinth
 num_generations = 4000
 sol_per_pop = 500
 num_parents_mating = 250
-num_genes = 30
+num_genes = 30  # number of steps allowed to take in the labyrinth to find the exit (20 is the shortest path)
 selection = "tournament"
 mutation = "random"
 mutation_prob = 0.15
 k_tournament = 10
 stop_criteria = "reach_1"
 
-"""Wagi punktów nagród & kar:"""
-bonus_point = 2  # do nagród
-pos_repeat_point = 1  # do kary za powtórzenie pozycji
-hitting_a_wall_point = 1.25  # do kary za zmarnowanie ruchu na odbicie się od ściany
-max_bonus = 10 * bonus_point  # maksymalnie 10 kroków czekamy w mecie, do której można dotrzeć w 20 z 30 kroków
+"""Weights of bonuses and punishments:"""
+bonus_point = 2
+pos_repeat_point = 1  # punishment for repeating a position
+hitting_a_wall_point = 1.25  # punishment for wasting a move to "bounce back" from a wall
+max_bonus = 10 * bonus_point  # if the GA follows the shortest path, it can wait up to 10 moves at the exit
 
 
 def fitness_fun_pygad(genetic_algorithm_instance, route, route_idx):
-    """Używamy metryki Taxi do ewaluacji tras przez labirynt. Dodatkowo, przydzielamy kary i nagrody za poszczególne
-    zachowania, aby trasy proponowane przez chromosomy były jak najbliższe tym faktycznym, po uwzględnieniu
-    "odbijania się" od ścian.
+    """We're using the Taxi metric to evaluate path through the labyrinth. Additionally, we give bonuses
+    and punishments for specific behaviours, so that path suggested by chromosomes were as close to actually possible
+    ones as possible, taking into account "bouncing back" from walls.
     """
-    position = {'y': 1, 'x': 1}  # zaczynamy w (1,1)
+    position = {'y': 1, 'x': 1}  # we're starting in (1,1)
 
-    """Aby uniknąć kłopotu z cechą 'mutable' słowników, zapamiętujemy w liście historii położeń kopię
-    początkowego stanu słownika położeń, zamiast przypisywać do listy dynamiczną strukturę danych.
-    """
+    """Not to encounter the 'mutable' attribute of dicts, in the position history list we save copies of positions
+    after each step:"""
     history = [copy.deepcopy(position)]
-    is_probem = 0  # początkowa wartość licznika problemów, do którego przydzielamy punkty kar
-    bonus = 0  # początkowa wartość bonusu, do której dodajemy punkty nagród
+    is_probem = 0  # starting number of unwanted behaviours
+    bonus = 0  # starting bonus value
 
-    for move in route:  # zmieniamy położenie w zależności od wykonanego ruchu
+    for move in route:  # changing position based on moves
 
         if position.get('x') == exit_labyrinth.get('x') and position.get('y') == exit_labyrinth.get('y') and move == 0:
-            bonus += bonus_point  # bonus za pozostanie w mecie
+            bonus += bonus_point  # bonus for staying at the exit
             continue
 
         new_y, new_x = position.get('y') + moves_mapping.get(move)[0], position.get('x') + moves_mapping.get(move)[1]
 
-        """Sprawdzamy, czy nowe współrzędne wskazują na dozwolone pole:"""
+        """Checkin, if the new position is an allowed field:"""
         if labyrinth[new_y, new_x] == 0:
             position['x'], position['y'] = new_x, new_y
             history.append(copy.deepcopy(position))
 
-            """Sprawdzamy, czy trzeba przydzielić karę za powtórzenie pozycji:"""
+            """Checking, if a punishment for repeating the position is required:"""
             if history.count(position) > 1:
                 is_probem += pos_repeat_point
-        else:  # pole, na które chce wejść chromosom, nie jest dozwolone!
+        else:  # field, onto which the chromosome want to step, is not allowed!
             is_probem += hitting_a_wall_point
 
-    """Najpierw obliczamy pomocnicze zmienne, dla czytelności:"""
+    """For the final fitness value, first we calculate some additional variables:"""
     x_distance = abs(exit_labyrinth.get('x') - position.get('x'))
     y_distance = abs(exit_labyrinth.get('y') - position.get('y'))
     sum_exit_coordinates = exit_labyrinth.get('x') + exit_labyrinth.get('y')
 
-    """Faktyczna wartość fitnessu, maksymalnie 1:"""
-    fitness_val = (sum_exit_coordinates - x_distance - y_distance) * 2  # użycie metryki taxi
-    fitness_val += bonus  # dodajemy punkty nagród za czekanie w mecie "do końca"
-    fitness_val -= is_probem  # odejmujemy punkty kar
+    """Actual fitness value, max 1:"""
+    fitness_val = (sum_exit_coordinates - x_distance - y_distance) * 2  # using Taxi metric
+    fitness_val += bonus  # adding bonus for waiting at the end
+    fitness_val -= is_probem  # subtracting punishments
     fitness_val = fitness_val / (sum_exit_coordinates * 2 + max_bonus)
 
     return fitness_val
@@ -257,17 +271,14 @@ def generator(args):
 
 
 def main_pygad():
-    """Główna funkcja wykonująca, aplikująca algorytm genetyczny do labiryntu zgodnie z ustawieniami w zmiennych
-    globalnych oraz z wykorzystaniem zdefiniowanych w skrypcie `projekt01_labirynt_wizualizacje.py` funkcji
-    do wizualizacji.
-    """
+    """PyGAD approach to solving the labyrinth"""
     fitness_list = []
     times = []
     output_list = []
-    generations_no = []  # nr generacji w danej iteracji, w której osiągnięto najlepsze rozwiązanie
+    generations_no = []  # number of generations in which a single GA found it's proposition for a solution
 
     for i in tqdm.tqdm(range(10)):
-        start = time()  # sprawdzamy czas na starcie
+        start = time()
 
         ga_instance = pygad.GA(
             gene_space=gene_space,
@@ -284,14 +295,14 @@ def main_pygad():
             K_tournament=k_tournament
         )
 
-        ga_instance.run()  # uruchamiamy algorytm genetyczny
-        end = time()  # mierzymy czas na koniec
+        ga_instance.run()
+        end = time()
         times.append(end - start)
 
-        """Ręcznie wizualizujemy historię fitnessu na przestrzeni generacji."""
-        fig, ax = plt.subplots()  # tworzymy osobną figurę na wykresy historii fitnessu!
-        fitness = ga_instance.best_solutions_fitness  # wartości na oś 0y
-        generations = list(range(len(fitness)))  # wartości na oś 0x
+        """Manually visualising fitness history through generations."""
+        fig, ax = plt.subplots()
+        fitness = ga_instance.best_solutions_fitness
+        generations = list(range(len(fitness)))
 
         ax.plot(generations, fitness, color="lime", linewidth=4, drawstyle='steps-post', label='Fitness')
 
@@ -302,46 +313,45 @@ def main_pygad():
         ax.grid(True)
         plt.show()
 
-        """Zapamiętujemy parametry rozwiązania:"""
+        """Remembering the solution:"""
         solution, solution_fitness, solution_idx = ga_instance.best_solution()
         generations_no.append(ga_instance.best_solution_generation)
         fitness_list.append(solution_fitness)
         output_list.append(solution)
 
-        """Wizualizujemy wyniki (trasy, które chromosomy chciały przejść)"""
+        """Visualising results (routes, which were tried by chromosomes)"""
         gif_filename = 'chromosome_animation' + str(i) + '.gif'
         picture_filename = 'chromosome_picture' + str(i) + '.png'
         see_route(labyrinth=labyrinth, moves_mapping=moves_mapping, steps=output_list[-1],
                   gif_filename=gif_filename, summary_filename=picture_filename)
 
-        """Wizualizujemy faktyczną trasę, z pominięciem kroków polegających na wejściu na pole niedozwolone"""
+        """Visualising the actual routes, not allowing to walk through the walls"""
         x, y = 1, 1
         history = []
 
         for step in output_list[-1]:
             new_y, new_x = y + moves_mapping.get(step)[0], x + moves_mapping.get(step)[1]
             if 0 <= new_y <= 11 and 0 <= new_x <= 11:
-                """Po zweryfikowaniu, że nowe współrzędne są wewnątrz labiryntu (tzn. mieszczą się w macierzy),
-                sprawdzamy, czy reprezentują dozwolone pole:
-                """
+                """After verifying that new coordinates are inside the labyrinth (in the matrix at all),
+                we check if they are an allowed field:"""
                 if labyrinth[new_y, new_x] == 0:
                     x, y = new_x, new_y
                     history.append(step)
                 else:
                     history.append(0)
             else:
-                print(f"Dostaliśmy współrzędne x={new_x} oraz y={new_y} poza labiryntem (przy wizualizacji).")
+                print(f"We got coordinates x={new_x} and y={new_y} from outside the labyrinth (when drawing).")
 
         gif_filename = 'actual_route_animation' + str(i) + '.gif'
         picture_filename = 'actual_route_picture' + str(i) + '.png'
         see_route(labyrinth=labyrinth, moves_mapping=moves_mapping, steps=history,
                   gif_filename=gif_filename, summary_filename=picture_filename)
 
-    print(f"Średni czas działania algorytmu genetycznego: {np.mean(times)}")
-    print(f"Średnia wartość f. fitnessu najlepszego rozwiązania: {np.mean(fitness_list)}")
-    print(f"Średnia liczba generacji do otrzymania najlepszego rozwiązania: {np.mean(generations_no)}")
+    print(f"Mean time of PyGAD's GA running: {np.mean(times)}")
+    print(f"Mean fitness value of the PyGAD's GA best solutions: {np.mean(fitness_list)}")
+    print(f"Mean number of generations in the PyGAD's GA to get the best solution: {np.mean(generations_no)}")
 
-    print(f"Historia wyników: \n")
+    print(f"Results history: \n")
     for j in range(len(output_list)):
         print(output_list[j])
 
@@ -367,4 +377,4 @@ def main_pyqkd():
 
 
 if __name__ == '__main__':
-    main_pyqkd()
+    main_pygad()
