cf5013c00bf3ca1f1235e2cb1ecfba1eefbeb6b3
Jakub Tomasz Gnyp <gnyp.jakub@gmail.com>
2025-09-23 00:19:05 +0200

    I was experimenting with custom managers and referencing attributes of objects in a shared space. Got a link to read about sharing states of complex objects

---
+++ genal/manager_test.py
@@ -0,0 +1,52 @@
+from multiprocessing.managers import SyncManager, BaseManager
+
+
+class SimpleObject:
+    a: float = None
+    b: float = None
+
+    def __init__(self, b):
+        self.b = b
+
+    def compute(self):
+        self.a = self.b * 2
+        return self.a
+
+
+class AdvancedObject(SimpleObject):
+    c: float = None
+
+    def __init__(self, b):
+        super().__init__(b)
+
+
+class MyBaseManager(BaseManager):
+    pass
+
+
+class MySyncManager(SyncManager):
+    pass
+
+
+MyBaseManager.register('SimpleObject', SimpleObject)
+MySyncManager.register('SimpleObject', SimpleObject)
+
+
+if __name__ == '__main__':
+    test0 = AdvancedObject(7)
+    a0 = test0.compute()
+    print(test0.a, a0)
+
+    with MyBaseManager() as manager1:
+        test1 = manager1.SimpleObject(7)
+        a1 = test1.compute()
+        print(a1)
+        print(test1.a)
+
+    with MySyncManager() as manager2:
+        test2 = manager2.SimpleObject(7)
+        a2 = test2.compute()
+        print(a2)
+        print(test2.a)
+
+    # TODO: https://stackoverflow.com/questions/72708828/how-to-create-the-attribute-of-a-class-object-instance-on-multiprocessing-in-pyt
